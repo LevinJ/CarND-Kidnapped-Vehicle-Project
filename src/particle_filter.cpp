67af7efc195745983f5a1900fe40a8d96de7eb9e
Levin Jian <jianzhirong@gmail.com>
2017-04-12 18:25:22 +0800

    add prediction step

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
+	// Set the number of particles. Initialize all particles to first position (based on estimates of
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
@@ -32,7 +32,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	normal_distribution<double> dist_y(y, std_y);
 	normal_distribution<double> dist_theta(theta, std_theta);
 
-	for(int i=1;i<=num_particles; i++){
+	for(int i=0;i < num_particles; i++){
 		Particle particle;
 		particle.id = i;
 		particle.x = dist_x(gen);
@@ -51,10 +51,18 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-	// TODO: Add measurements to each particle and add random Gaussian noise.
+	// Add measurements to each particle and add random Gaussian noise.
 	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
+	for(int i=0; i< num_particles;i++){
+		Particle &particle = particles[i];
+		double new_theta = particle.theta + yaw_rate * delta_t;
+		particle.x = particle.x + (velocity/yaw_rate) * (sin(new_theta) - sin(particle.theta));
+		particle.y = particle.y + (velocity/yaw_rate) *(cos(particle.theta) -  cos(new_theta));
+		particle.theta = new_theta;
+		//maybe need to add noise(std_pos) later on, though for now I don't need to do so
+	}
 
 }
 
